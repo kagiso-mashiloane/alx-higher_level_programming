3d6d6ef1dc3a86d0bfae3d4e7cf020e0d614b603
kagiso-mashiloane <kagiso.s.mashiloane@gmail.com>
2024-05-25 01:02:55 +0200

    0x10-python-network_0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -s -X PUT -d "user_id=98" -H "Origin: You got me!" -L 0.0.0.0:5000/catch_me
+# This script makes a request to 0.0.0.0:5000/catch_me that gets the response message "You got me!".
+
+# -s: Silent mode (don't show progress)
+# -L: Follow redirects
+# -X PUT: Use PUT method
+# -d "user_id=98": Send data with user_id=98
+# -H "Origin: School": Set Origin header to "School"
+curl -s -L -X PUT -d "user_id=98" -H "Origin: School" 0.0.0.0:5000/catch_me
+
