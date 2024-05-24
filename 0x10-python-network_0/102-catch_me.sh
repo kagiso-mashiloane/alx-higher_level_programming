4928fc35fd6f009c8aac46aed4a16f8947445cea
kagiso-mashiloane <kagiso.s.mashiloane@gmail.com>
2024-05-25 01:08:50 +0200

    0x10-python-network_0

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Makes a request to 0.0.0.0:5000/catch_me that gets the message "School".
-curl -s -X PUT -d "user_id=98" -H "Origin: School!" -L 0.0.0.0:5000/catch_me
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -s -X PUT -d "user_id=98" -H "Origin: You got me!" -L 0.0.0.0:5000/catch_me
+
